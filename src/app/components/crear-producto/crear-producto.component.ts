334201ed429947b8236b99916b27539af50d2f0c
Ricardo Reyes <ricar0421.reyes@gmail.com>
2024-07-23 11:50:41 -0600

    commit nuevo

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from 'express';
+
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.scss'
 })
--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-listar-productos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './listar-productos.component.html',
   styleUrl: './listar-productos.component.scss'
 })
